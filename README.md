4aadfb92d8402bba194a8ee60097722659daea52
kuznetsovandrey76 <and.rey.q@yandex.ru>
2019-11-07 22:21:31 +0300

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+npm init -y
+npm install --save hbs express body-parser mysql
+
+https://metanit.com/web/nodejs/8.4.php
+https://github.com/mysqljs/mysql
+https://www.youtube.com/watch?v=v0t42xBIYIs
---
+++ app.js
@@ -0,0 +1,119 @@
+const mysql      = require('mysql');
+const express = require("express");
+const bodyParser = require("body-parser");
+ 
+const app = express();
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+
+const connection = mysql.createConnection({
+  host     : 'akuznetsov.beget.tech',
+  user     : 'akuznetsov_sql',
+  database : 'akuznetsov_sql',
+  password : '***' // ИЗМЕНИТЬ
+});
+
+// ПОДКЛЮЧЕНИЕ
+// connection.connect(function(err) {
+//     if (err) {
+//         return console.error('error connecting: ' + err.message);
+//     }
+
+//     console.log('Подключение к серверу MySQL, id - ' + connection.threadId);
+// });
+
+// СОЗДАЕМ ТАБЛИЦУ
+// const sql = `create table if not exists users(
+// id int primary key auto_increment,
+// name varchar(255) not null,
+// age int not null
+// )`;
+// connection.query(sql, function(err, results) {
+//     if(err) console.log(err);
+//     else console.log("Таблица создана");
+// });
+
+app.set("view engine", "hbs");
+
+
+// ВЫБОРКА
+// connection.query('SELECT * FROM people', function(error, result, fields){
+//     console.log(error);
+//     console.log(result); // собственно данные
+//     console.log(fields); // мета-данные полей 
+// });
+
+
+// ОТКЛЮЧАЕМСЯ
+// connection.end(function(err) {
+//     if (err) {
+//         return console.log("Ошибка: " + err.message);
+//     }
+//     console.log("Подключение закрыто");
+// });
+
+// получение списка пользователей
+app.get("/", function(req, res){
+    connection.query("SELECT * FROM users", function(err, data) {
+      if(err) return console.log(err);
+      res.render("index.hbs", {
+          users: data
+      });
+    });
+});
+// возвращаем форму для добавления данных
+app.get("/create", function(req, res){
+    res.render("create.hbs");
+});
+// получаем отправленные данные и добавляем их в БД 
+app.post("/create", urlencodedParser, function (req, res) {
+         
+    if(!req.body) return res.sendStatus(400);
+    const name = req.body.name;
+    const age = req.body.age;
+    connection.query("INSERT INTO users (name, age) VALUES (?,?)", [name, age], function(err, data) {
+      if(err) return console.log(err);
+      res.redirect("/");
+    });
+});
+ 
+// получем id редактируемого пользователя, получаем его из бд и отправлям с формой редактирования
+app.get("/edit/:id", function(req, res){
+  const id = req.params.id;
+  connection.query("SELECT * FROM users WHERE id=?", [id], function(err, data) {
+    if(err) return console.log(err);
+     res.render("edit.hbs", {
+        user: data[0]
+    });
+  });
+});
+// получаем отредактированные данные и отправляем их в БД
+app.post("/edit", urlencodedParser, function (req, res) {
+         
+  if(!req.body) return res.sendStatus(400);
+  const name = req.body.name;
+  const age = req.body.age;
+  const id = req.body.id;
+   
+  connection.query("UPDATE users SET name=?, age=? WHERE id=?", [name, age, id], function(err, data) {
+    if(err) return console.log(err);
+    res.redirect("/");
+  });
+});
+ 
+// получаем id удаляемого пользователя и удаляем его из бд
+app.post("/delete/:id", function(req, res){
+          
+  const id = req.params.id;
+  connection.query("DELETE FROM users WHERE id=?", [id], function(err, data) {
+    if(err) return console.log(err);
+    res.redirect("/");
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// app.listen(3000, function(){
+//   console.log("Сервер ожидает подключения...");
+// });
